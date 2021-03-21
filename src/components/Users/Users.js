b214be50a7aa2a1c6f32b3a1cfd09a2848612a6f
toronir <toronit5@gmail.com>
2021-03-21 17:18:32 +0100

    Social network
    Update

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,45 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 import styles from "./Users.module.css";
 import {NavLink} from "react-router-dom";
-import cn from "classnames";
+import Paginator from "../common/Paginator/Paginator";
 
 
 let Users = (props) => {
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [];
-    for (let i=1; i <= pagesCount; i++)
-    {
-        pages.push(i);
-    }
-    debugger
-    let portionCount = Math.ceil(pagesCount/props.portionSize);
-    let [portionNumber, setPortionNumber] = useState(1);
-    let leftPortionPageNumber = (portionNumber - 1) * props.portionSize +1;
-    let rightPortionPageNumber = portionNumber * props.portionSize;
-
-
     return (
         <div>
-            <div className={styles.paginator}>
-                {portionNumber > 1 &&
-                <button onClick={()=> {setPortionNumber(portionNumber-1)}}>PREV</button>}
-                {pages
-                    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-                    .map((p) => {
-                    return  <span className={ cn({
-                    [styles.selectedPage]: props.currentPage === p
-                    },styles.pageNumber ) }
-                            key={p}
-                            onClick={(e) => {
-                                props.onPageChanged(p);
-                            }}> {p} </span>
+            <Paginator currentPage={props.currentPage} onPageChanged={props.onPageChanged} totalUsersCount={props.totalUsersCount}
+                       pageSize={props.pageSize} portionSize={props.portionSize}/>
 
 
-
-                })}
-                {portionCount > portionNumber &&
-                <button onClick={() =>{ setPortionNumber(portionNumber+1)}}>NEXT</button> }
-            </div>
             {props.users.map((u) => (
                 <div key={u.id}>
           <span>
